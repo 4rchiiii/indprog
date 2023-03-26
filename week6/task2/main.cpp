8e4cdf357510033a4fd82922ca29a1c7ae0bd64e
chstkv <112215737+4rchiiii@users.noreply.github.com>
2023-03-26 23:43:32 +0300

    Add files via upload

---
+++ week6/task2/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "subcone.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int size;
+	cout << "������� ���-�� ��������� �������\n";
+	cin >> size;
+
+	subcone* C = new subcone[size];
+
+	cout << "\n";
+	cout << "������� ��������� ����� � ������� {x y z R H subH}\n";
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << "������� ������ ��������� ������ ����� " << i + 1 << ": ";
+		cin >> C[i];
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << "\n";
+		cout << "������ ������ ����� " << i + 1 << ":\n";
+		cout << C[i] << "\n";
+		cout << "S = " << C[i].area() << "\n";
+		cout << "V = " << C[i].volume() << "\n";
+	}
+
+	return 0;
+}
